bbe46d4b84cb65e107f2201c47ae80311d46676f
SUSANA ALARCON <skatherine1990@gmail.com>
2023-09-17 16:58:37 -0500

    def ordenar_fila(matriz, fila):     matriz[fila] = sorted(matriz[fila])
    # Matriz de ejemplo (3x3)
    matriz_ejemplo2 = [
        [5, 12, 1],
        [8, 4, 10],
        [6, 3, 7]
    ]
    
    fila_a_ordenar = 0
    
    print("Matriz original:")
    for fila in matriz_ejemplo2:
        print(fila)
    
    ordenar_fila(matriz_ejemplo2, fila_a_ordenar)
    
    print(f"\nMatriz con la fila {fila_a_ordenar + 1} ordenada:")
    for fila in matriz_ejemplo2:
        print(fila)

---
+++ temperatura promedio de ciudades.py
@@ -0,0 +1,34 @@
+def temperatura_promedio(datos):
+    """
+    Calcula la temperatura promedio de múltiples ciudades durante un período de tiempo.
+
+    Args:
+        datos (dict): Un diccionario que contiene los datos de temperatura por ciudad y semana.
+                      La estructura debe ser: {'ciudad1': [temp1_semana1, temp1_semana2, ...],
+                                               'ciudad2': [temp2_semana1, temp2_semana2, ...],
+                                               ...}
+
+    Returns:
+        dict: Un diccionario con las temperaturas promedio por ciudad.
+    """
+    temperaturas_promedio = {}
+
+    for ciudad , temperaturas_semana in datos.items ():
+        # Calcula la temperatura promedio para cada ciudad
+        promedio = sum ( temperaturas_semana ) / len ( temperaturas_semana )
+        temperaturas_promedio [ ciudad ] = promedio
+
+    return temperaturas_promedio
+
+
+# Ejemplo de uso:
+datos_temperatura = {
+    'Ciudad QUITO': [ 25 , 27 , 26 , 28 ] ,
+    'Ciudad GUAYAQUIL': [ 22 , 23 , 21 , 24 ] ,
+    'Ciudad CUENCA': [ 29 , 30 , 28 , 31 ]
+}
+
+temperaturas_promedio = temperatura_promedio ( datos_temperatura )
+
+for ciudad , promedio in temperaturas_promedio.items ():
+    print ( f"La temperatura promedio en {ciudad} es: {promedio}°C" )
