bda5525f7ee4855ced781e9b3fd0a60d411c9ac1
Susana Alarcón <skatherine1990@gmail.com>
2023-09-24 21:11:58 -0500

    def ordenar_fila(matriz, fila):     matriz[fila] = sorted(matriz[fila])
    # Matriz de ejemplo (3x3)
    matriz_ejemplo2 = [
        [5, 12, 1],
        [8, 4, 10],
        [6, 3, 7]
    ]
    
    fila_a_ordenar = 0
    
    print("Matriz original:")
    for fila in matriz_ejemplo2:
        print(fila)
    
    ordenar_fila(matriz_ejemplo2, fila_a_ordenar)
    
    print(f"\nMatriz con la fila {fila_a_ordenar + 1} ordenada:")
    for fila in matriz_ejemplo2:
        print(fila)

---
+++ calcular_descuento.py
@@ -0,0 +1,22 @@
+def calcular_descuento(monto_total , porcentaje_descuento=10):
+    descuento = (monto_total * porcentaje_descuento) / 100
+    monto_final = monto_total - descuento
+    return descuento , monto_final
+
+
+# Llamada a la función proporcionando solo el monto total de la compra
+monto_compra1 = 100
+descuento1 , monto_final1 = calcular_descuento ( monto_compra1 )
+print ( f"Monto de compra: ${monto_compra1}" )
+print ( f"Descuento aplicado: ${descuento1}" )
+print ( f"Monto final a pagar: ${monto_final1}" )
+
+# Llamada a la función proporcionando tanto el monto total de la compra como el porcentaje de descuento
+monto_compra2 = 200
+porcentaje_descuento2 = 15
+descuento2 , monto_final2 = calcular_descuento ( monto_compra2 , porcentaje_descuento2 )
+print ( "\nLlamada con monto y porcentaje personalizado:" )
+print ( f"Monto de compra: ${monto_compra2}" )
+print ( f"Porcentaje de descuento: {porcentaje_descuento2}%" )
+print ( f"Descuento aplicado: ${descuento2}" )
+print ( f"Monto final a pagar: ${monto_final2}" )
