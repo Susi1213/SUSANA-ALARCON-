e2a89d26f7593d0a982059066a7d4458b5366483
susi1213 <skatherine1990@gmail.com>
2023-12-08 20:58:53 -0500

    def ordenar_fila(matriz, fila):     matriz[fila] = sorted(matriz[fila])
    # Matriz de ejemplo (3x3)
    matriz_ejemplo2 = [
        [5, 12, 1],
        [8, 4, 10],
        [6, 3, 7]
    ]
    
    fila_a_ordenar = 0
    
    print("Matriz original:")
    for fila in matriz_ejemplo2:
        print(fila)
    
    ordenar_fila(matriz_ejemplo2, fila_a_ordenar)
    
    print(f"\nMatriz con la fila {fila_a_ordenar + 1} ordenada:")
    for fila in matriz_ejemplo2:
        print(fila)

--- 1 ejemplo Búsqueda en Arreglo Multidimensional.py
+++ 1 ejemplo Búsqueda en Arreglo Multidimensional.py
@@ -1,23 +1,1 @@
-def buscar_valor(matriz, valor):
-    for i in range(len(matriz)):
-        for j in range(len(matriz[i])):
-            if matriz[i][j] == valor:
-                return i, j
-    return None
-
-
-# Matriz de ejemplo (3x3)
-matriz_ejemplo2 = [
-    [11, 22, 33],
-    [44, 55, 66],
-    [77, 88, 99]
-]
-
-valor_a_buscar = 88
-posicion = buscar_valor(matriz_ejemplo2, valor_a_buscar)
-
-if posicion:
-    fila, columna = posicion
-    print(f"Encontrado: El valor {valor_a_buscar} se encuentra en la fila {fila + 1}, columna {columna + 1}.")
-else:
-    print(f"No encontrado: El valor {valor_a_buscar} no se encuentra en la matriz.")
+def buscar_valor(matriz, valor):    for i in range(len(matriz)):        for j in range(len(matriz[i])):            if matriz[i][j] == valor:                return i, j    return None# Matriz de ejemplo (3x3)matriz_ejemplo2 = [    [11, 22, 33],    [44, 55, 66],    [77, 88, 99]]valor_a_buscar = 88posicion = buscar_valor(matriz_ejemplo2, valor_a_buscar)if posicion:    fila, columna = posicion    print(f"Encontrado: El valor {valor_a_buscar} se encuentra en la fila {fila + 1}, columna {columna + 1}.")else:    print(f"No encontrado: El valor {valor_a_buscar} no se encuentra en la matriz.")
